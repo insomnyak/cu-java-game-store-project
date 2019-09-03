7f63d6243295481c9bab863c5e9d8d4f44c2107a
insomnyak <rene@insomnyak.com>
2019-09-03 10:13:52 -0400

    first commit

---
+++ src/main/java/com/company/ReneSerulleU1Capstone/ReneSerulleU1CapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.company.ReneSerulleU1Capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReneSerulleU1CapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReneSerulleU1CapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/ReneSerulleU1Capstone/ReneSerulleU1CapstoneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.company.ReneSerulleU1Capstone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReneSerulleU1CapstoneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
